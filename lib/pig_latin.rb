b928d505a47cafabf4fb00fb483a5fead6abd6a2
RandyDavis <randy_davis@outlook.com>
2014-07-29 12:44:49 -0500

    Update pig_latin.rb to be a single method and update spec test to match and pass.

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,30 +1,54 @@
-require "pig_latin/version"
+# require "pig_latin/version"
 
 module PigLatin
   # Your code goes here...
-  def self.check_input(word)
+  def self.translate(word)
     if word.class == String
       true
+        if word =~ /\A[yY]/
+          top = word.slice!(0)
+          word = word + top + "ay"
+        elsif word !~ /\A[aAeEiIoOuU]/
+          top = word.slice!(0)
+          word = word + top + "ay"
+        elsif word =~ /\A[aAeEiIoOuU]/
+          word + "way"
+        end
+    elsif word == nil
+      return "Please enter a word"
+    else
+      return "Read the instructions and start again"
     end
   end
+      
+        
+        
+        
 
-  def self.first_letter_y(word)
-    if word =~ /\A[yY]/
-      top = word.slice!(0)
-      word = word + top + "ay"
-    end
-  end
 
-  def self.first_letter_consonant(word)
-    if word !~ /\A[aAeEiIoOuU]/
-      top = word.slice!(0)
-      word = word + top + "ay"
-    end
-  end
+  # def self.check_input(word)
+  #   if word.class == String
+  #     true
+  #   end
+  # end
+
+  # def self.first_letter_y(word)
+  #   if word =~ /\A[yY]/
+  #     top = word.slice!(0)
+  #     word = word + top + "ay"
+  #   end
+  # end
+
+  # def self.first_letter_consonant(word)
+  #   if word !~ /\A[aAeEiIoOuU]/
+  #     top = word.slice!(0)
+  #     word = word + top + "ay"
+  #   end
+  # end
   
-  def self.first_letter_vowel(word)
-    if word =~ /\A[aAeEiIoOuU]/
-      word + "way"
-    end
-  end
+  # def self.first_letter_vowel(word)
+  #   if word =~ /\A[aAeEiIoOuU]/
+  #     word + "way"
+  #   end
+  # end
 end
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -2,30 +2,24 @@ require 'spec_helper.rb'
 
 describe 'Pig Latin' do
 
-  describe '.check_input' do
+  describe '.translate' do
     it "checks to make sure that a word has been entered" do
-      result = PigLatin.check_input("slip")
-      expect(result.class).to eq(TrueClass)
+      result = PigLatin.translate("slip")
+      expect(result.class).to eq(String)
     end
-  end
-
-  describe '.first_letter_y' do
+ 
     it "checks that the inputed word starts with 'y'" do
-      result = PigLatin.first_letter_y("you")
+      result = PigLatin.translate("you")
       expect(result).to eq("ouyay")
     end
-  end
-
-  describe '.consonant' do
+ 
     it "checks to see that the inputed word doesn't start with a vowel and relocates consonant cluster while adding 'ay' to the end" do
-      result = PigLatin.first_letter_consonant("slip")
+      result = PigLatin.translate("slip")
       expect(result).to eq("lipsay")
     end
-  end
-
-  describe '.vowel' do
+  
     it "checks to see that the inputed word starts with a vowel and adds 'way' to the end" do
-      result = PigLatin.first_letter_vowel("egg")
+      result = PigLatin.translate("egg")
       expect(result).to eq("eggway")
     end
   end
